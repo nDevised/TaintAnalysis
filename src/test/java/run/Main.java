215857d33c3e450e1fa7afbad55d49632372d748
Dave Kaushik <90827405+nDevised@users.noreply.github.com>
2023-12-03 03:32:04 -0700

    Update Main.java

--- src/test/java/run/Main.java
+++ src/test/java/run/Main.java
@@ -4,7 +4,6 @@ import analysis.IFDSTaintAnalysisProblem;
 import analysis.data.DFF;
 import boomerang.scene.jimple.BoomerangPretransformer;
 import heros.InterproceduralCFG;
-import org.junit.Test;
 import soot.*;
 import soot.jimple.toolkits.ide.JimpleIFDSSolver;
 import soot.jimple.toolkits.ide.icfg.JimpleBasedInterproceduralCFG;
@@ -18,8 +17,76 @@ import java.util.function.Supplier;
 import java.util.stream.Collectors;
 public class Main {
     // The IFDSTestSetupClass is being re-used for the purp
+    protected static JimpleIFDSSolver<?, ?> solver = null;
     public static void main(String[] args) {
-        // Must input args otherwise send message to user
+        if (args.length != 1) {
+            System.err.println("Usage: Main <class-to-analyze>");
+            return;
+        }
+
+        String targetClass = args[0];
+        Main main = new Main();
+        main.run(targetClass);
+
+    }
+
+    public void run(String targetClass){
+        JimpleIFDSSolver<?, ? extends InterproceduralCFG<Unit, SootMethod>> analysis = executeStaticAnalysis(targetClass);
+        Set<String> defaultIDEResult = getResult(analysis);
+        displayResults(defaultIDEResult);
+
+    }
+
+    private void setupSoot(String targetClass) {
+
+        G.reset();
+        String userdir = System.getProperty("user.dir");
+        String javaHome = System.getProperty("java.home");
+
+        String sootCp = userdir + File.separator + "target" + File.separator + "test-classes"+ File.pathSeparator + javaHome + File.separator +"lib"+File.separator+"rt.jar";
+        Options.v().set_soot_classpath(sootCp);
+
+        // We want to perform a whole program, i.e. an interprocedural analysis.
+        // We construct a basic CHA call graph for the program
+        Options.v().set_whole_program(true);
+        Options.v().setPhaseOption("cg.cha", "on");
+        Options.v().setPhaseOption("cg", "all-reachable:true");
+
+        Options.v().set_no_bodies_for_excluded(true);
+        Options.v().set_allow_phantom_refs(true);
+        Options.v().setPhaseOption("jb", "use-original-names:true");
+        Options.v().setPhaseOption("jb.ls", "enabled:false");
+        Options.v().set_prepend_classpath(false);
+
+        Scene.v().addBasicClass("java.lang.StringBuilder");
+        SootClass c = Scene.v().forceResolve(targetClass, SootClass.BODIES);
+        if (c != null) {
+            c.setApplicationClass();
+        }
+        Scene.v().loadNecessaryClasses();
+
+    }
+
+    protected SootMethod getEntryPointMethod() {
+        for (SootClass c : Scene.v().getApplicationClasses()) {
+            for (SootMethod m : c.getMethods()) {
+                if (!m.hasActiveBody()) {
+                    continue;
+                }
+                if (m.getName().equals("entryPoint") || m.toString().contains("void main(java.lang.String[])")) {
+                    return m;
+                }
+            }
+        }
+        throw new IllegalArgumentException("Method does not exist in scene!");
+    }
+
+    private void registerSootTransformers() {
+        Transform transform = new Transform("wjtp.ifds", createAnalysisTransformer());
+        PackManager.v().getPack("wjtp").add(transform);
+    }
+
+    protected Transformer createAnalysisTransformer() {
         List<SootMethodRef> sources = new ArrayList<>();
         List<SootMethodRef> sinks = new ArrayList<>();
         SootClass sourceClass = new SootClass("target.taint.internal.SourceClass");
@@ -35,9 +102,64 @@ public class Main {
         sinks.add(sink1);
         sinks.add(sink2);
 
-        // Create the Scene Transformer
-
+        return new SceneTransformer() {
+            @Override
+            protected void internalTransform(String phaseName, Map<String, String> options) {
+                JimpleBasedInterproceduralCFG icfg = new JimpleBasedInterproceduralCFG(false);
+                IFDSTaintAnalysisProblem problem = new IFDSTaintAnalysisProblem(icfg, sources, sinks);
+                @SuppressWarnings({"rawtypes", "unchecked"})
+                JimpleIFDSSolver<?, ?> solver = new JimpleIFDSSolver<>(problem);
+                solver.solve();
+                Main.solver = solver; // check, this might be problematic
+            }
+        };
     }
+
+    private Set<String> getResult(Object analysis) {
+        SootMethod m = getEntryPointMethod();
+        Map<DFF, Integer> res = null;
+        Set<String> result = new HashSet<>();
+        if (analysis instanceof JimpleIFDSSolver) {
+            JimpleIFDSSolver solver = (JimpleIFDSSolver) analysis;
+            res = (Map<DFF, Integer>) solver.resultsAt(m.getActiveBody().getUnits().getLast());
+        }
+        for (Map.Entry<DFF, Integer> e : res.entrySet()) {
+            result.add(e.getKey().toString());
+        }
+        return result;
+    }
+
+    private void displayResults(Set<String> defaultIDEResult) {
+        if (defaultIDEResult.isEmpty()) {
+            System.out.println("No tainted elements found.");
+        } else {
+            System.out.println("Tainted elements:");
+            for (String element : defaultIDEResult) {
+                System.out.println(element);
+            }
+        }
+    }
+
+    protected JimpleIFDSSolver<?, ?> executeStaticAnalysis(String targetTestClassName) {
+        setupSoot(targetTestClassName);
+        registerSootTransformers();
+        executeSootTransformers();
+        if (solver == null) {
+            throw new NullPointerException("Something went wrong solving the IFDS problem!");
+        }
+        return solver;
+    }
+
+    private void executeSootTransformers() {
+        //Apply all necessary packs of soot. This will execute the respective Transformer
+        PackManager.v().getPack("cg").apply();
+        // Must have for Boomerang
+        BoomerangPretransformer.v().reset();
+        BoomerangPretransformer.v().apply();
+        PackManager.v().getPack("wjtp").apply();
+    }
+
+
 
 }
 
